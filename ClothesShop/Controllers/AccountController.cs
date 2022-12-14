3e959d5b80699ca8807f8e83a8f4d42d70a12b02
Ptrk12 <random12@onet.pl>
2022-12-14 19:37:28 +0100

    Added LoginView

---
+++ ClothesShop/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using ClothesShop.Data;
+using ClothesShop.Data.ViewModels;
+using ClothesShop.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothesShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            var result = new LoginVM();
+            return View(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAdress);
+            if(user != null)
+            {
+                var passwordCheck = await _userManager.CheckPasswordAsync(user,loginVM.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user,loginVM.Password,false,false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Clothes");
+                    }
+                }
+                TempData["Error"] = "Try again";
+                return View(loginVM);
+            }
+            TempData["Error"] = "Try again";
+            return View(loginVM);
+        }
+    }
+}
---
+++ ClothesShop/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothesShop.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name ="Email adress")]
+        [Required(ErrorMessage ="Email adress is required")]
+        public string EmailAdress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
